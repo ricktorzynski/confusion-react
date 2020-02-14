67ff197d5f67ce77b3d42bf199df3c02a1ee8fa7
ricktorzynski <ricktorzynski@gmail.com>
2020-02-14 00:12:38 -0500

    Removing renderDish from MenuComponent - redundant

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -19,23 +19,6 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card key={dish.id}>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else
-      return (
-        <div></div>
-      );
-  }
-
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
